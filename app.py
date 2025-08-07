c5a5b061e82e733b0653a3dd7f5a3a5a8f095494
Amogh Turaga <70780931+JoelWillams2021@users.noreply.github.com>
2025-08-07 09:10:22 -0400

    Added hard coded year

--- app.py
+++ app.py
@@ -113,7 +113,7 @@ def recommend():
     if not genre:
         return jsonify({'error': 'Missing genre'}), 400
 
-    current_year = pd.Timestamp.now().year
+    current_year = 2025
     min_year = current_year - 15
 
     # substring match anywhere in the original comma-list
